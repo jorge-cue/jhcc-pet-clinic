a2c060bbeb8f01cba12f836d70284e447986caeb
Jorge Cue <jorge.cue@gmail.com>
2020-08-24 20:36:32 -0500

    Initial commit

---
+++ src/main/java/com/springguru/jhccpetclinic/JhccPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.springguru.jhccpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JhccPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JhccPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springguru/jhccpetclinic/JhccPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springguru.jhccpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JhccPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
