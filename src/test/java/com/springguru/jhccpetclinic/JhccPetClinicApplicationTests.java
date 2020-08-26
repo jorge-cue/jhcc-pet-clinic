8a1a3e1b3786e6795ed0cda448c09310db40814a
Jorge Cue <jorge.cue@gmail.com>
2020-08-25 19:58:03 -0500

    Remove JUnit4 support and add smokeTest checking /actuator/health

--- src/test/java/com/springguru/jhccpetclinic/JhccPetClinicApplicationTests.java
+++ src/test/java/com/springguru/jhccpetclinic/JhccPetClinicApplicationTests.java
@@ -1,13 +1,33 @@
 package com.springguru.jhccpetclinic;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class JhccPetClinicApplicationTests {
 
+    @Autowired
+    MockMvc mockMvc;
+
     @Test
-    void contextLoads() {
+    void smokeTest() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                .get("/actuator/health")
+                .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().json("{\"status\":\"UP\"}"));
     }
 
 }
